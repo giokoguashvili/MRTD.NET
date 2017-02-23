1e9b23c068dd2b80556a777c3d17db20b808947c
kogoia <kogo27@gmail.com>
2017-02-23 14:01:05 +0400

    added readerNames code

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PCSC;
 
 namespace HelloWord
 {
@@ -9,6 +10,14 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {
+            var contextFactory = ContextFactory.Instance;
+
+            using (var context = contextFactory.Establish(SCardScope.System))
+            {
+                var readerNames = context.GetReaders();
+                readerNames.ToList().ForEach(item => Console.WriteLine(item));
+                Console.ReadKey();
+            }
         }
     }
 }
