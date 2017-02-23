8a5d53dda1bdb9389ec8ae41779af28654caba72
kogoia <kogo27@gmail.com>
2017-02-23 20:49:42 +0400

    beautified the code

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -10,14 +10,9 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {
-
-
-
             //var contextFactory = ContextFactory.Instance;
-
             var cardContext = new SCardContext();
             cardContext.Establish(SCardScope.System);
-
 
             SCardMonitor monitor = new SCardMonitor(cardContext, SCardScope.System);
             // Point the callback function(s) to the pre-defined method MyCardInsertedMethod.
@@ -25,23 +20,18 @@ namespace HelloWord
             // Start to monitor the reader
             monitor.Start("ACS CCID USB Reader 0");
 
-
             using (cardContext)
             {
                 var readerNames = cardContext.GetReaders();
                 readerNames
                     .ToList()
                     .ForEach(readerName => Console.WriteLine(readerName));
-
                 readerNames
                     .ToList()
                     .ForEach(readerName =>
                     {
                         using (var reader = new SCardReader(cardContext))
                         {
-
-
-
                             var cardError = reader.Connect(readerName, SCardShareMode.Shared, SCardProtocol.Any);
                             if (cardError == SCardError.Success)
                             {
@@ -56,7 +46,6 @@ namespace HelloWord
                                             out proto, // contains the currently used communication protocol
                                             out atr); //            
 
-
                                 Console.WriteLine("Connected with protocol {0} in state {1}", proto, state);
                                 Console.WriteLine("Card ATR: {0}", BitConverter.ToString(atr));
                             }
@@ -68,45 +57,11 @@ namespace HelloWord
                     });
                 Console.ReadKey();
             }
-
         }
 
         static void CardInsertEventHandler(object sender, PCSC.CardStatusEventArgs e)
         {
-
             Console.WriteLine("Reader name: {0}\nCard ART: {1}", e.ReaderName, BitConverter.ToString(e.Atr));
-            //using (var reader = new SCardReader(cardContext))
-            //{
-
-
-
-            //    var cardError = reader.Connect(readerName, SCardShareMode.Shared, SCardProtocol.Any);
-            //    if (cardError == SCardError.Success)
-            //    {
-            //        string[] names;
-            //        SCardProtocol proto;
-            //        SCardState state;
-            //        byte[] atr;
-
-            //        var sc = reader.Status(
-            //                    out readerNames, // contains the reader name(s)
-            //                    out state, // contains the current state (flags)
-            //                    out proto, // contains the currently used communication protocol
-            //                    out atr); //            
-
-
-            //        Console.WriteLine("Connected with protocol {0} in state {1}", proto, state);
-            //        Console.WriteLine("Card ATR: {0}", BitConverter.ToString(atr));
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Error message: {0}\n", SCardHelper.StringifyError(cardError));
-            //    }
-            //}
-
-
-            //Console.WriteLine("Connected with protocol {0} in state {1}", proto, state);
-            //Console.WriteLine("Card ATR: {0}", BitConverter.ToString(atr));
         }
     }
 }
