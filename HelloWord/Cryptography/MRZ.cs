888aa4b901541f9370db824fdbc6f20b99337575
kogoia <kogo27@gmail.com>
2017-03-07 17:38:03 +0400

    added MRZ class

---
+++ HelloWord/Cryptography/MRZ.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWord.Cryptography
+{
+    public class MRZ
+    {
+        private readonly string _documentNumber;
+        private readonly string _dateOfBirth;
+        private readonly string _dateOfExpiry;
+        public MRZ()
+        {
+
+        }
+
+        public string Info()
+        {
+            return String.Format(
+                    "{0}{1}{2}",
+                    this._documentNumber,
+                    this._dateOfBirth,
+                    this._dateOfExpiry
+                );
+        }
+    }
+}
