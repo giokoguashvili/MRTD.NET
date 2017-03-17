0382050ea540eeaa1d09cc97d675ac320af4e34d
kogoia <kogo27@gmail.com>
2017-03-17 15:19:00 +0400

    added KseedIc class impl with test

---
+++ HelloWord/Cryptography/Kic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HelloWord.Infrastructure;
+
+namespace HelloWord.Cryptography
+{
+    public class Kic : IBinary
+    {
+        public byte[] Bytes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
