ac58267e87338b0f24cfe2b929ced4b46a6fdb2f
kogoia <kogo27@gmail.com>
2017-03-18 15:18:50 +0400

    fixed KseedIc test

--- UnitTests/KseedIcTests.cs
+++ UnitTests/KseedIcTests.cs
@@ -8,7 +8,7 @@ namespace UnitTests
     public class KseedIcTests
     {
         [Test]
-        [TestCase("0036D272F5C350ACAC50C3F572D23600x", "0B795240CB7049B01C19B33E32804F0B", "0B4F80323EB3191CB04970CB4052790B")]
+        [TestCase("0036D272F5C350ACAC50C3F572D23600", "0B795240CB7049B01C19B33E32804F0B", "0B4F80323EB3191CB04970CB4052790B")]
         public void Calculate_XOR_of_KIFD_and_KIC(string act, string kIfd, string kIc)
         {
             Assert.AreEqual(
