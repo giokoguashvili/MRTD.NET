280dafbd0fa5e8cb2b469eb88633a362f9942b96
kogoia <kogo27@gmail.com>
2017-03-24 18:04:57 +0400

    added Decrypt_DO87_Data_from_ProtectedResponseApdu test

--- UnitTests/DO87Tests.cs
+++ UnitTests/DO87Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HelloWord.Cryptography;
 using HelloWord.Infrastructure;
 using HelloWord.SecureMessaging.CommandDO;
 using HelloWord.SecureMessaging.DO;
@@ -34,10 +35,35 @@ namespace UnitTests
                     new Hex(
                         new DO87(
                             new FkKSenc()
-                        )
-                        .FromProtectedResponseApdu(
+                        ).FromProtectedResponseApdu(
                             new BinaryHex("8709019FF0EC34F9922651990290008E08AD55CC17140B2DED9000")
                         )
+                    ).ToString()
+                );
+        }
+
+        [Test]
+        [TestCase(
+                "60145F01",
+                "8709019FF0EC34F9922651990290008E08AD55CC17140B2DED9000"
+            )]
+        [TestCase(
+                "04303130365F36063034303030305C026175",
+                "871901FB9235F4E4037F2327DCC8964F1F9B8C30F42C8E2FFF224A990290008E08C8B2787EAEA07D749000"
+            )]
+        public void Decrypt_DO87_Data_from_ProtectedResponseApdu(string act, string protectedResponseApdu)
+        {
+            Assert.AreEqual(
+                    // add padding on protectedrResponseApdu decrypted data
+                    new Hex(new PaddedData(new BinaryHex(
+                        act                     
+                    ))).ToString(),
+                    new Hex(
+                        new DO87(
+                            new FkKSenc()
+                        ).FromProtectedResponseApdu(
+                            new BinaryHex(protectedResponseApdu)
+                        ).DecryptedData()
                     ).ToString()
                 );
         }
