3c89e98ac99aaa4cf5892afac031aaab41d06495
kogoia <kogo27@gmail.com>
2017-03-30 22:12:04 +0400

    added BER_TLV test

--- UnitTests/BER-TLV/TLVTests.cs
+++ UnitTests/BER-TLV/TLVTests.cs
@@ -9,13 +9,56 @@ namespace UnitTests.BER_TLV
     public class TLVTests
     {
         [Test]
-        public void TestMethod1()
+        public void Parse_BER_TLV()
         {
-            var parsetTvl = new BerTLV(
+            var parsedTvl = new BerTLV(
                                 new BinaryHex("6F1A840E315041592E5359532E4444463031A5088801025F2D02656E")
                             );
-            var data = parsetTvl.Data;
-            Assert.Fail();
+
+            Assert.AreEqual(
+                    "6F",
+                    parsedTvl.Tag
+                );
+            Assert.AreEqual(
+                    "840E315041592E5359532E4444463031A5088801025F2D02656E",
+                    parsedTvl.Val
+                );
+
+            Assert.AreEqual(
+                    "84",
+                    parsedTvl.Data[0].Tag
+                );
+            Assert.AreEqual(
+                    "315041592E5359532E4444463031",
+                     parsedTvl.Data[0].Val
+                );
+
+            Assert.AreEqual(
+                    "A5",
+                    parsedTvl.Data[1].Tag
+                );
+            Assert.AreEqual(
+                    "8801025F2D02656E",
+                     parsedTvl.Data[1].Val
+                );
+
+            Assert.AreEqual(
+                    "88",
+                    parsedTvl.Data[1].Data[0].Tag
+                );
+            Assert.AreEqual(
+                    "02",
+                     parsedTvl.Data[1].Data[0].Val
+                );
+
+            Assert.AreEqual(
+                    "5F2D",
+                    parsedTvl.Data[1].Data[1].Tag
+                );
+            Assert.AreEqual(
+                    "656E",
+                     parsedTvl.Data[1].Data[1].Val
+                );
         }
     }
 }
