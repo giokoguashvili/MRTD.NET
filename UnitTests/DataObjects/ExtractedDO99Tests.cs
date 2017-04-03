e019ae38707038314d9761d9bab0095e4f6e1f19
kogoia <kogo27@gmail.com>
2017-04-03 11:42:34 +0400

    added Extract_DO99_from_protectedResponseApdu tests

--- UnitTests/DataObjects/ExtractedDO99Tests.cs
+++ UnitTests/DataObjects/ExtractedDO99Tests.cs
@@ -9,13 +9,16 @@ namespace UnitTests
     public class ExtractedDO99Tests
     {
         [Test]
-        public void Extract_DO99_from_protectedResponseApdu()
+        [TestCase("99029000", "990290008E08FA855A5D4C50A8ED9000")]
+        [TestCase("99029000", "8709019FF0EC34F9922651990290008E08AD55CC17140B2DED9000")]
+        [TestCase("99029000", "871901FB9235F4E4037F2327DCC8964F1F9B8C30F42C8E2FFF224A990290008E08C8B2787EAEA07D749000")]
+        public void Extract_DO99_from_protectedResponseApdu(string exc, string protectedResponseApdu)
         {
             Assert.AreEqual(
-                    "99029000",
+                    exc,
                     new Hex(
                         new ExtractedDO99(
-                            new BinaryHex("8709019FF0EC34F9922651990290008E08AD55CC17140B2DED9000")
+                            new BinaryHex(protectedResponseApdu)
                         )
                     ).ToString()
                 );
