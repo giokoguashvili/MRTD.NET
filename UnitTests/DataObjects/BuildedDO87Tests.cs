fa92bdae6ac1a976f72b37b4fc39c1642d3696bd
kogoia <kogo27@gmail.com>
2017-04-03 11:35:59 +0400

    added Extract_DO87_from_protectedResponseApdu tests

--- UnitTests/DataObjects/BuildedDO87Tests.cs
+++ UnitTests/DataObjects/BuildedDO87Tests.cs
@@ -28,34 +28,5 @@ namespace UnitTests
                     ).ToString()
                 );
         }
-
-        [Test]
-        [TestCase(
-                "60145F01",
-                "8709019FF0EC34F9922651990290008E08AD55CC17140B2DED9000"
-            )]
-        [TestCase(
-                "04303130365F36063034303030305C026175",
-                "871901FB9235F4E4037F2327DCC8964F1F9B8C30F42C8E2FFF224A990290008E08C8B2787EAEA07D749000"
-            )]
-        public void Decrypt_DO87_Data_from_ProtectedResponseApdu(string act, string protectedResponseApdu)
-        {
-            Assert.AreEqual(
-                    // add padding on protectedrResponseApdu decrypted data
-                    new Hex(
-                        new Padded(
-                            new BinaryHex(
-                                act
-                            )
-                        )   
-                    ).ToString(),
-                    new Hex(
-                        new DecryptedProtectedResponseApdu(
-                            new BinaryHex(protectedResponseApdu),
-                            new FkKSenc()
-                        )
-                    ).ToString()
-                );
-        }
     }
 }
--- UnitTests/DataObjects/ExtractedDO87Tests.cs
+++ UnitTests/DataObjects/ExtractedDO87Tests.cs
@@ -9,13 +9,15 @@ namespace UnitTests
     public class ExtractedDO87Tests
     {
         [Test]
-        public void Extract_DO87_from_protectedResponseApdu()
+        [TestCase("8709019FF0EC34F9922651", "8709019FF0EC34F9922651990290008E08AD55CC17140B2DED9000")]
+        [TestCase("871901FB9235F4E4037F2327DCC8964F1F9B8C30F42C8E2FFF224A", "871901FB9235F4E4037F2327DCC8964F1F9B8C30F42C8E2FFF224A990290008E08C8B2787EAEA07D749000")]
+        public void Extract_DO87_from_protectedResponseApdu(string exc, string protectedResponseApdu)
         {
             Assert.AreEqual(
-                    "8709019FF0EC34F9922651",
+                    exc,
                     new Hex(
                         new ExtractedDO87(
-                            new BinaryHex("8709019FF0EC34F9922651990290008E08AD55CC17140B2DED9000")
+                            new BinaryHex(protectedResponseApdu)
                         )
                     ).ToString()
                 );
