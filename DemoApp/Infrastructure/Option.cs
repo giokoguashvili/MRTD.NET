a4cd0489a8beab6ed915cf5fbc25fbb2298b3304
kogoia <kogo27@gmail.com>
2017-04-09 20:29:59 +0400

    added optionEnumerator class

--- DemoApp/Infrastructure/Option.cs
+++ DemoApp/Infrastructure/Option.cs
@@ -8,36 +8,6 @@ using System.Threading.Tasks;
 
 namespace DemoApp.Infrastructure
 {
-    public class OptionEnumerator<T> : IEnumerable<T>
-    {
-        private readonly T[] _data;
-
-        public OptionEnumerator(T element)
-            : this(new T[] { element })
-        {
-        }
-
-        public OptionEnumerator()
-            : this(new T[0])
-        { }
-
-        public OptionEnumerator(T[] data)
-        {
-            _data = data;
-        }
-
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return ((IEnumerable<T>) _data).GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-    }
-
     public class Option<T> : IOption<T>
     {
         private readonly T[] _data;
---
+++ DemoApp/Infrastructure/OptionEnumerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp.Infrastructure
+{
+    public class OptionEnumerator<T> : IEnumerable<T>
+    {
+        private readonly T[] _data;
+
+        public OptionEnumerator(T element)
+            : this(new T[] { element })
+        {
+        }
+
+        public OptionEnumerator()
+            : this(new T[0])
+        { }
+
+        public OptionEnumerator(T[] data)
+        {
+            _data = data;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_data).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
