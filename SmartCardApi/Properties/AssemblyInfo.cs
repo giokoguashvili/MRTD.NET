2be3fdf56d6d5d2808b09bb5abcb70b9d4152cd6
kogoia <kogo27@gmail.com>
2017-04-24 16:48:39 +0400

    commit

--- SmartCardApi/Properties/AssemblyInfo.cs
+++ SmartCardApi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SmartCardApi.Properties")]
+[assembly: AssemblyTitle("SmartCardApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SmartCardApi.Properties")]
+[assembly: AssemblyProduct("SmartCardApi")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
